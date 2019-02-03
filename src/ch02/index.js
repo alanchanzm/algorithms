a99ef6c92c203b20958d97d8ee33f462d2984d51
Alan Chan <alanchanzm@outlook.com>
2019-02-03 15:44:23 +0800

    refactor(ch02): less

--- src/ch02/index.js
+++ src/ch02/index.js
@@ -2,4 +2,8 @@ function exch(arr, i, j) {
   [arr[i], arr[j]] = [arr[j], arr[i]];
 }
 
-export { exch };
+function less(a, b, fn = (x, y) => x - y) {
+  return fn(a, b) < 0;
+}
+
+export { exch, less };
--- src/ch02/selection.js
+++ src/ch02/selection.js
@@ -1,11 +1,11 @@
-import { exch } from './index';
+import { exch, less } from './index';
 
 function selection(arr) {
   const N = arr.length;
   for (let i = 0; i < N; i += 1) {
     let min = i;
     for (let j = i + 1; j < N; j += 1) {
-      if (arr[j] < arr[min]) min = j;
+      if (less(arr[j], arr[min])) min = j;
     }
     exch(arr, i, min);
   }
